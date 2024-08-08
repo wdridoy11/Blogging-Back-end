68ddd5c65012047a61d3bd654691f0921ef8899b
Ridoy sharif <wdridoy11@gmail.com>
2024-08-08 20:43:57 +0600

    favorite blog delete api create succesful

--- index.js
+++ index.js
@@ -96,7 +96,12 @@ async function run() {
         res.send(result);
     })
 
-
+    app.delete('/favorite-blog/:id', async (req,res)=>{
+        const id = req.params.id;
+        const filter = { _id : new ObjectId(id)};
+        const result = await favoriteBlogCollection.deleteOne(filter);
+        res.send(result);
+    })
 
 
 
